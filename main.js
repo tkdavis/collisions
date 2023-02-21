08d09480a217ab82b2cb55106289a6be8d78503f
Tyler Davis <datadazer@gmail.com>
2023-02-20 17:41:44 -0800

    extremely naive intersections to start

--- main.js
+++ main.js
@@ -21,17 +21,30 @@ function initCircles() {
         y: 500,
         radius: radius,
         color: "green",
-        velocity: {x: 1, y: -1, magnitude: 1}
+        velocity: {x: -1, y: -1, magnitude: 1}
     }
 
     circles.push(circle1, circle2);
 }
 
 function updatePhysics() {
+    if (getDistance(circles[0], circles[1]) < radius * 2) {
+        circles.forEach( circle => {
+            circle.color = "red";
+        })
+    }
+
     circles.forEach( circle => {
         circle.x += circle.velocity.x * circle.velocity.magnitude;
         circle.y += circle.velocity.y * circle.velocity.magnitude;
     });
+}
+
+function getDistance(obj1, obj2) {
+    let xDistance = obj2.x - obj1.x;
+    let yDistance = obj2.y - obj1.y;
+
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
 function draw() {
