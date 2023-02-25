48d085a0be30fb271d06976b68862d10634349d2
Tyler Davis <datadazer@gmail.com>
2023-02-25 09:43:45 -0800

    adjust canvas to client dimensions

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById('main-canvas');
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
 const offscreenCanvas = canvas.transferControlToOffscreen();
 const worker = new Worker(
     new URL('offscreenCanvas.js', import.meta.url),
